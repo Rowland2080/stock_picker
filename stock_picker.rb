cda889638aed863654bb9bbcfc10615e518da78b
Rowland2080 <Rowlandkelechi.kr@gmail.com>
2021-03-02 23:53:34 +0100

    Added profit margin

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,12 +1,17 @@
 my_array = [17,3,6,9,15,8,6,1,10]
 def stock_picker(picks)
   best_dates = []
+  best_stock_prices = []
   picks.each_with_index do |pick, index|
-    best_dates << index if pick == picks.max
-    best_dates.unshift(index) if pick == picks.min
+    if pick == picks.max
+      best_dates << index
+      best_stock_prices << pick
+    elsif pick == picks.min
+      best_dates.unshift(index)
+      best_stock_prices.unshift(pick) 
+    end
   end
-  p best_dates
-  puts "Therefore, i strongly recommend that you buy stocks on day #{best_dates[0]} and sell on day #{best_dates[1]} to get maximum profit "
+  puts "Therefore, i strongly recommend that you buy stocks on day #{best_dates[0]} and sell on day #{best_dates[-1]} to get maximum profit of $#{best_stock_prices[-1] - best_stock_prices[0]}."
 end
 
 stock_picker(my_array)
